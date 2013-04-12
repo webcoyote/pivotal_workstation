26feaf9df1af85befb3cd55ec98d3c2c40632e43
Patrick Wyatt <pat@codeofhonor.com>
2013-04-12 20:55:51 +0000

    update rbenv installer for Linux

--- attributes/rbenv.rb
+++ attributes/rbenv.rb
@@ -1,11 +1,12 @@
 unless node["rbenv"]
-  node.default["rbenv"]= {
+  node.default["rbenv"] = {
     "rubies" => {
-        "ree-1.8.7-2012.01" => {},
-        "1.8.7-p352" => {},
-        "1.9.2-p290" => {},
-        "1.9.3-p125" => { :env => {"CC" => "clang"}}
-      },
-    "default_ruby" => "1.9.3-p125"
+        # "ree-1.8.7-2012.01" => {},
+        # "1.8.7-p352" => {},
+        # "1.9.2-p290" => {},
+        # "1.9.3-p125" => { :env => {"CC" => "clang"}}
+        "2.0.0-p0" => {}
+    },
+    "default_ruby" => "2.0.0-p0"
   }
 end
--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -1,10 +1,31 @@
-include_recipe "pivotal_workstation::git"
-
 ::RBENV_HOME = "#{WS_HOME}/.rbenv"
-::RBENV_COMMAND = "/usr/local/bin/rbenv"
+::RBENV_COMMAND = "#{RBENV_HOME}/bin/rbenv"
 
-brew "rbenv"
-brew "ruby-build"
+if node["platform"] == "mac_os_x"
+  include_recipe "pivotal_workstation::git"
+  brew "rbenv"
+  brew "ruby-build"
+else
+  git "#{RBENV_HOME}" do
+    repository 'git://github.com/sstephenson/rbenv.git'
+    user WS_USER
+    group WS_USER
+    reference 'master'
+    action :sync
+  end
+  directory "#{RBENV_HOME}/plugins" do
+    owner WS_USER
+    group WS_USER
+    mode '0755'
+  end
+  git "#{RBENV_HOME}/plugins/ruby-build" do
+    repository 'git://github.com/sstephenson/ruby-build.git'
+    user WS_USER
+    group WS_USER
+    reference 'master'
+    action :sync
+  end
+end
 
 pivotal_workstation_bash_it_enable_feature "plugins/rbenv"
 
@@ -16,6 +37,6 @@ end
 
 execute "making #{node["rbenv"]["default_ruby"]} with rbenv the default" do
   not_if { node["rbenv"]["default_ruby"].nil? }
-  command "rbenv global #{node["rbenv"]["default_ruby"]}"
+  command "#{RBENV_COMMAND} global #{node["rbenv"]["default_ruby"]}"
   user WS_USER
 end
