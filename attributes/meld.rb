a76e87bb1a85ccb7f62acc092edd6dd07b9eb5a5
Patrick Wyatt <pat@codeofhonor.com>
2013-10-05 06:17:57 +0000

    install meld from source

---
+++ attributes/meld.rb
@@ -0,0 +1,9 @@
+# Install mode: package or source
+node.default['meld']['install_method'] = 'source'
+
+# Package install: get moldy old version from aptitude or yum
+
+# Source install
+default['meld']['source']['url']      = 'http://ftp.gnome.org/pub/GNOME/sources/meld/1.8/meld-1.8.1.tar.xz'
+default['meld']['source']['checksum'] = 'f1f13deeb74625a97f97c311eb00665d4a883f0ce705d3299b9c02d50c4ac9c5'
+
--- recipes/meld.rb
+++ recipes/meld.rb
@@ -1,4 +1,25 @@
-package "meld"
+if node['meld']['install_method'] == 'package'
+
+  package "meld"
+
+else
+
+  filename = File.basename(node['meld']['source']['url'])
+  srcfile = "#{Chef::Config[:file_cache_path]}/#{filename}"
+  remote_file srcfile do
+    source node['meld']['source']['url']
+    checksum node['meld']['source']['checksum']
+  end
+
+  directory "/opt/meld" do
+    mode 0755
+  end
+
+  execute "extract meld" do
+    command "tar --strip-components=1 -xf '#{srcfile}' -C /opt/meld"
+  end
+
+end
 
 # Configure git unless otherwise requested
 config = node["meld"] || {}
