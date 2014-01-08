de1671003c88f75787dad87177835d85e3e90c58
Patrick Wyatt <pat@codeofhonor.com>
2014-01-08 21:18:40 +0000

    updated meld to 1.8.3

--- attributes/meld.rb
+++ attributes/meld.rb
@@ -4,6 +4,6 @@ node.default['meld']['install_method'] = 'source'
 # Package install: get moldy old version from aptitude or yum
 
 # Source install
-default['meld']['source']['url']      = 'http://ftp.gnome.org/pub/GNOME/sources/meld/1.8/meld-1.8.1.tar.xz'
-default['meld']['source']['checksum'] = 'f1f13deeb74625a97f97c311eb00665d4a883f0ce705d3299b9c02d50c4ac9c5'
+default['meld']['source']['url']      = 'http://ftp.gnome.org/pub/GNOME/sources/meld/1.8/meld-1.8.3.tar.xz'
+default['meld']['source']['checksum'] = '42ba344cacfe4c5afaf5db8a43df2c877ec9e5b8e66621b92f2f9e0dcbf6fed7'
 
